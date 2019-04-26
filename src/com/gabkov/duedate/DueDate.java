cae524f67ae54ed361b39b78046971fb37794a7d
Gábor Kovács <gabor950601@gmail.com>
2019-04-26 17:13:17 +0200

    deleted docs

--- src/com/gabkov/duedate/DueDate.java
+++ src/com/gabkov/duedate/DueDate.java
@@ -3,18 +3,11 @@ package com.gabkov.duedate;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
-/**
- * In my solution I use the work hours as a LinkedList. First I shift the values until the first element will be the
- * submit hour. Next I left shift as many hours are left-over (the not whole days).
- * Lastly I add the days to the submitted date and return the Due Date with the shifted hour.
- */
 
 public class DueDate {
 
     public LocalDateTime dueDateCalculator(LocalDateTime submitDate, int hours) throws NotWorkingHoursException, InvalidHoursException {
-        int submitHour = submitDate.getHour();
-
-        if (submitHour < 9 || (submitHour >= 17))
+        if (submitDate.getHour() < 9 || (submitDate.getHour() >= 17))
             throw new NotWorkingHoursException("Work hours are between 9AM-5PM, please try again tomorrow.");
         if (hours < 1) throw new InvalidHoursException("Hours must be at least 1 or positive");
 
@@ -33,6 +26,5 @@ public class DueDate {
         return result;
     }
 
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) { }
 }
